0acdcca83ad59a31a405a7b36b7bf8dcb854c7de
Eduardo Villalpando <villalpe5@gmail.com>
2026-08-21 12:37:18 -0600

    Refactor database settings to use environment variables
    Updated database configuration to use environment variables for name, user, host, and port.

--- backend/settings.py
+++ backend/settings.py
@@ -126,14 +126,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'musicodigo',
-        'USER': 'villalpe',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
-        'HOST': 'musicodigo-identifier.cqxjlv3zpj7v.us-east-1.rds.amazonaws.com',
-        'PORT':'5432',
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
