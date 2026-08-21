7f11b857543836a50630b322cb87af922a8d92f0
Eduardo Villalpando <villalpe5@gmail.com>
2026-08-21 12:43:31 -0600

    Switch database configuration to use dj_database_url

--- backend/settings.py
+++ backend/settings.py
@@ -124,14 +124,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
-    }
+    "default": dj_database_url.config(
+        default="postgres://postgres:postgres@localhost:5432/postgres",
+        conn_max_age=600
+    )
 }
 
 # Password validation
