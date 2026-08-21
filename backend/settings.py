fdd970260eeb620f3484491948b3119b24076c77
Eduardo Villalpando <villalpe5@gmail.com>
2026-08-21 12:47:43 -0600

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 import os
+import dj_database_url
 
 
 # Quick-start development settings - unsuitable for production
