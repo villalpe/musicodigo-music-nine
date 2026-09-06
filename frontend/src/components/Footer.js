608279abb6d8484cf888da2cb11d5ed28a37aed2
Eduardo Villalpando <villalpe5@gmail.com>
2026-09-06 15:15:43 -0600

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -129,8 +129,7 @@ function Footer() {
                 <div class="row pt-2">
                     <div class="col-12">
                         <span class="text-left text-light h5" style={{ fontFamily: 'rm_typerighter'}}>
-                            Copyright &copy; 2022  
-                            FOOTER TEST 2026-09-06 12:00 12345
+                            © 2022 Diseño operativo: Diego Tinajero, desarrollo: Eduardo Villalpando, diseño Visual: Jéssica Rangel. Código fuente bajo licencia GNU GPL v3.0.
                         </span>
                     </div>
                 </div>
