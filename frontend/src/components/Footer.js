9da9068693c9d7f2d09c13c871e5cd6c6aa86b9d
Eduardo Villalpando <villalpe5@gmail.com>
2026-09-07 10:13:52 -0600

    Fix Footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -127,7 +127,7 @@ function Footer() {
             <div class="container">
                 <div class="row pt-2">
                     <div class="col-12">
-                        <span class="text-left text-light h5" style={{ fontFamily: 'rm_typerighter'}}>
+                        <span className="text-left text-light h5" style={{ fontFamily: 'rm_typerighter'}}>
                             © 2022 Diseño operativo: Diego Tinajero, desarrollo: Eduardo Villalpando, diseño Visual: Jéssica Rangel. código fuente bajo licencia GNU GPL v3.0.
                         </span>
                     </div>
